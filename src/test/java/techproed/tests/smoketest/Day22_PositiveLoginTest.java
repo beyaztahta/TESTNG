53d9ee4a2451f13c017440005adb43a1039b4b72
Elif Calık <beyaztahta0@gmail.com>
2023-02-09 23:20:41 +0300

    09-02-2023

--- src/test/java/techproed/tests/smoketest/Day22_PositiveLoginTest.java
+++ src/test/java/techproed/tests/smoketest/Day22_PositiveLoginTest.java
@@ -2,6 +2,7 @@ package techproed.tests.smoketest;
 
 
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 import techproed.pages.BlueCarRentHomePage;
 import techproed.pages.BlueCarRentLoginPage;
@@ -15,15 +16,20 @@ public class Day22_PositiveLoginTest {
     @Test
     public void test01() {
 
+        Reporter.log("Sayfaya git ");
         //1. url'ye git
         Driver.getDriver().get(ConfigReader.getProperty("blueRental_url"));
 
         //2. Login butonuna bas
         BlueCarRentHomePage objeHomePage = new BlueCarRentHomePage();
+        BlueCarRentLoginPage objeLoginPage = new BlueCarRentLoginPage();
+        BlueCarRentalDashboard dashboardObje = new BlueCarRentalDashboard();
+
+        Reporter.log("Login butonuna tıkla.");
         objeHomePage.homePageLoginButton.click();
 
         //3. username ve sifre alanlarini gecerli verilerle doldur
-        BlueCarRentLoginPage objeLoginPage = new BlueCarRentLoginPage();
+        Reporter.log("Giris bilgilerini gir.");
         objeLoginPage.username.sendKeys(ConfigReader.getProperty("blueRental_username"));
         objeLoginPage.password.sendKeys(ConfigReader.getProperty("blueRental_password"));
 
@@ -31,8 +37,9 @@ public class Day22_PositiveLoginTest {
         objeLoginPage.loginBtn.click();
 
         //5. basarili bir sekilde giris yapildigini dogrula
-        BlueCarRentalDashboard dashboardObje = new BlueCarRentalDashboard();
+        Reporter.log("Giris yapıldıgını verify et.");
         Assert.assertEquals("Jack Nicholson", dashboardObje.userNameText.getText());
+        Reporter.log("Driver ı kapat.");
         Driver.closeDriver();
     }
 }
--- src/test/java/techproed/utilities/ListenersRetry.java
+++ src/test/java/techproed/utilities/ListenersRetry.java
@@ -5,7 +5,7 @@ import org.testng.ITestResult;
 public class ListenersRetry implements IRetryAnalyzer {
     // bu sinif sadece kalan testleri tekrar calistirir. kac defa calistirmak istedigimizi  maxretycount da yazabiliriz.
     private int retryCount = 0;
-    private static final int maxRetryCount = 2;
+    private static final int maxRetryCount = 1;
     @Override
     public boolean retry(ITestResult result) {
         if (retryCount < maxRetryCount) {
